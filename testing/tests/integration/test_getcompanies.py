1083933c8e8f90903d139c73b808a9ce532e9af1
noor367 <nh@lappy.local>
2025-04-03 22:06:57 +1100

    Fixed getcompanies

--- testing/tests/integration/test_getcompanies.py
+++ testing/tests/integration/test_getcompanies.py
@@ -1,12 +1,6 @@
 import pytest
 from unittest.mock import patch
-from flask import Flask, jsonify
-from index import app 
-
-##########################################################################
-# /GETCOMPANIES ROUTE
-# THE FOLLOWING TESTS CHECK THE /GETCOMPANIES ROUTE
-##########################################################################
+from index import app
 
 @pytest.fixture
 def client():
@@ -15,57 +9,30 @@ def client():
     with app.test_client() as client:
         yield client
 
-@patch("index.run_sql")
-def test_getCompanies_valid_industry(mock_run_sql, client):
+def test_getCompanies_valid_industry(client):
     """Test the /getCompanies route with a valid industry."""
-    # Simulate a successful SQL query
-    mock_run_sql.return_value = [{"company_name": "PrimeCity Investment PLC"}]
 
     # Make a GET request to the /getCompanies route
-    response = client.get(
-        "/getCompanies?industry=Airlines"
-    )
+    response = client.get("/getCompanies?industry=Biofuels")
 
-    # Assertions
     assert response.status_code == 200
-    assert response.json == {"company_name": "PrimeCity Investment PLC"}
+    assert response.json == '{"companies": ["Pervasive Commodities Ltd", "Clearway Energy Operating LLC", "Tulasee Bio-Ethanol Ltd", "Laxai Pharma Ltd", "Duke Energy Florida LLC", "Cardinal Ethanol LLC", "Red Trail Energy LLC", "Tsaker New Energy Tech Co Ltd", "Indo Acidatama Tbk PT", "Willing New Energy Co Ltd", "Global Bioenergies SA", "Asian Insulators PCL", "Able Global Bhd", "AI Energy PCL", "Borosil Renewables Ltd", "Granite Falls Energy LLC", "China New Energy Ltd", "Biosev SA", "Scandinavian Biogas Fuels International AB", "Cortus Energy AB", "Montauk Renewables Inc", "Global Clean Energy Holdings Inc", "Industry Source Consulting Inc", "Active Energy Group PLC", "Southern Online Bio Technologies Ltd", "Longyan Zhuoyue New Energy Co Ltd", "Leaf Resources Ltd", "Timah Resources Ltd", "Affinity Energy and Health Ltd", "Taronis Fuels Inc", "Solarvest Bioenergy Inc", "BBGI PCL", "808 Renewable Energy Corp", "Green Earth Institute Co Ltd", "Evolution Fuels Inc", "Renewal Fuels Inc", "StemCell Institute Inc", "Triboron International AB", "Aggregated Micro Power Holdings PLC", "Mission Newenergy Ltd", "New Global Energy Inc", "Archaea Energy Inc", "Crimson Bioenergy Ltd", "Duke Energy Progress LLC", "Bluefire Renewables Inc", "Duke Energy Indiana LLC", "Western Asset Variable Rate Strategic Fund Inc", "Western Asset Emerging Markets Debt Fund Inc", "C2e Energy Inc", "GeoBio Energy Inc", "Verbio Vereinigte Bioenergie AG", "Alto Ingredients Inc", "Energy Absolute PCL", "CropEnergies AG", "Enviva Inc", "Gevo Inc", "Raizen SA", "Misen Energy AB (publ)", "Asia Biomass PCL", "Heron Lake BioEnergy LLC", "Fintec Global Bhd", "Velocys PLC", "Wave Sync Corp", "Blackstone Senior Floating Rate 2027 Term Fund"]}'
 
 def test_getCompanies_no_industry(client):
     """Test the /getCompanies route with no industry."""
     # Make a GET request to the /getCompanies route with no industry
-    response = client.get(
-        "/getCompanies"
-    )
-  
+    response = client.get("/getCompanies")
+
     assert response.status_code == 400
-    assert response.data.decode() == "Invalid params, please specify an industry. See https://unswcse.atlassian.net/wiki/spaces/SCAI/pages/964329628/Available+Companies+and+Industries for allowed industries."
-  
-@patch("index.run_sql_raw")
-def test_getCompanies_invalid_industry(mock_run_sql, client):
+    assert response.data.decode() == (
+        "Invalid params, please specify an industry. See https://unswcse.atlassian.net/wiki/spaces/SCAI/pages/964329628/Available+Companies+and+Industries for allowed industries."
+    )
+
+def test_getCompanies_invalid_industry(client):
     """Test the /getCompanies route with an invalid industry."""
-    # Simulate a successful SQL query
-    mock_run_sql.return_value = []
 
     # Make a GET request to the /getCompanies route with an invalid industry
-    response = client.get(
-        "/getCompanies?industry=CABANA"
-    )
+    response = client.get("/getCompanies?industry=CABANA")
 
-    # Assertions
     assert response.status_code == 400
     assert response.data.decode() == "No companies found for 'CABANA'. See https://unswcse.atlassian.net/wiki/spaces/SCAI/pages/964329628/Available+Companies+and+Industries for allowed industries"
-
-def test_sql_exception(client):
-    """Test the /getIndustry route when a SQL exception occurs."""
-    # Simulate an exception in the run_sql function
-    with patch("index.run_sql") as mock_run_sql:
-        mock_run_sql.side_effect = Exception("SQL Exception occurred.")
-        
-        # Make a GET request to the /getIndustry route
-        response = client.get(
-            "/getIndustry?company=PrimeCity+Investment+PLC"
-        )
-
-        # Assertions
-        assert response.status_code == 500
-        assert response.data.decode() == "SQL Exception occurred."
