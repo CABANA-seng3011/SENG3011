12e5d102748f5512b679e859a087e56f6a369491
noor367 <gh.noor367@gmail.com>
2025-04-27 17:53:19 +1000

    Edited conftest.py

--- testing/conftest.py
+++ testing/conftest.py
@@ -3,6 +3,7 @@ from index import app
 
 @pytest.fixture
 def client():
-    app = index(testing=True)
+    """Fixture to create a test client for the Flask app."""
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
